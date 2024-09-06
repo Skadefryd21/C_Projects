bf67b4234d02a9fc8c346b1005fb6889992d7210
Nikolai 'Møller <nikolaimoeller@hotmail.dk>
2024-09-06 21:48:37 +0200

    moved around some syntax

--- Mario/mario.c
+++ Mario/mario.c
@@ -5,29 +5,27 @@ void printObstacle(int height);
 
 int main(void)
 {
-    int height;
+    int height = 0;
     short condition = 1;
 
     while(condition == 1){
         printf("How tall should the obstacle be: \n");
         int result = scanf("%i", &height);
-        if(result == 1){
-            if(height > 0 && height < 9){
-            printObstacle(height);
-            condition = 0;
-        }
-        else{
-            //Integer has size of 4 bytes.
-            if(sizeof(height) != sizeof(int)){
-                printf("Input must be of type Integer.");
-            }
-            else{
-                printf("Input must be from 1 to and with 8.");
-            }
-        }
+        if(result != 1 || height < 1 || height > 8){
+            system("cls");
+            while (getchar() != '\n'){
+            printf("Input must be of type Integer & must be within the range of 1 to and with 8, try again. \n");
         }
     }
+        else{
+            printObstacle(height);
+            condition = 0;
+        }  
+    }
+    return 0;
 }
+
+
 
 void printObstacle(int height){
     for(int i = 0; i < height; i++){
