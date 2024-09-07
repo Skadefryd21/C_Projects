6f4a6a6fa613190cfd0d56931f801e08d327b2f1
Nikolai 'Møller <nikolaimoeller@hotmail.dk>
2024-09-07 14:15:48 +0200

    moved if statement and added continue to make code more effecient.

--- Mario/mario.c
+++ Mario/mario.c
@@ -12,21 +12,21 @@ int main(void)
     while(condition == 1){
         printf("How tall should the obstacle be: \n");
         int result1 = scanf("%i", &height);
-
-        printf("How wide should the hole of the obstacle be: \n");
-        int result2 = scanf("%i", &middle);
-
         if(result1 != 1 || height < 1 || height > 8){
             int c;
             system("cls");
             printf("Input for height must be of type Integer & must be within the range of 1 to and with 8, try again. \n");
             while (c = getchar() != '\n'){}
+            continue;
         }
+        printf("How wide should the hole of the obstacle be: \n");
+        int result2 = scanf("%i", &middle);
         if(result2 != 1 || middle < 2 || middle > 4){
             int c;
             system("cls");
             printf("Input for width must be of type Integer & must be within the range of 2 to and with 4, try again. \n");
             while (c = getchar() != '\n'){}
+            continue;
         }
         else{
             printObstacle(height, middle);
